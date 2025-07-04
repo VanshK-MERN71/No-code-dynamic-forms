c5317f8f2d7a2ad0b2973ad473121e1c4812c560
VanshK-MERN71 <vanshkhanna9140@gmail.com>
2025-07-04 16:37:17 +0530

    Updated Home.jsx remove FormBuider.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,10 @@
 import React from "react";
-import FormBuilder from "./../componets/FormBuilder";
 import CustomFormBuilder from "../componets/CustomFormBuilder";
 
 function Home() {
   return (
     <div>
       <CustomFormBuilder />
-      <FormBuilder />
     </div>
   );
 }
